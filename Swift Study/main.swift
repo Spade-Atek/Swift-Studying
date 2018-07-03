4cfebb0027d9efff44f98b2c270ee3428ee6158a
Spade-Atek <xieyutai@icloud.com>
2018-07-03 23:47:47 +0800

    Initial Commit

---
+++ Swift Study/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Study
+//
+//  Created by Spade-Atek on 2018/7/3.
+//  Copyright © 2018年 Spade-Atek. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
